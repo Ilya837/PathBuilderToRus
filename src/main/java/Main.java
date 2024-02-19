af011a4560e1b83f16f23f78e189bdde511e48a4
Mikerin Ilya <mikerin-2002@mail.ru>
2024-02-19 22:58:02 +0300

    add translate ancestry and heritage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,26 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.interactive.form.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private static HashMap<String,String> ancestryDic;
 
     public static void listFields(PDDocument doc, PDDocument doc2,String newFileName) throws Exception {
         PDDocumentCatalog catalog = doc.getDocumentCatalog();
@@ -35,7 +51,75 @@ public class Main {
 
     }
 
+    private static String translateAncestry(String ancestry){
+
+
+
+        String[] a = ancestry.split(" \\(");
+
+        a[0] = ancestryDic.get(a[0]);
+        a[1] = ancestryDic.get(a[1].substring(0,a[1].length()-1));
+
+
+        return a[0] + " \u0028 " + a[1] + " )";
+    }
+
+    public static void translate(PDDocument doc,String newCharacterPath) throws IOException {
+
+        PDFont font = PDType0Font.load(doc, new FileInputStream(Main.class.getResource("arial.ttf").getPath()), false);
+
+        PDDocumentCatalog catalog = doc.getDocumentCatalog();
+        PDAcroForm form = catalog.getAcroForm();
+        List<PDField> fields = form.getFields();
+
+        PDResources dr = form.getDefaultResources();
+
+        COSName fontName = dr.add(font);
+
+        for(PDField field: fields) {
+
+            if (field instanceof PDTextField)
+            {
+                PDTextField textField = (PDTextField) field;
+                String da = textField.getDefaultAppearance();
+
+                // replace font name in default appearance string
+                Pattern pattern = Pattern.compile("\\/(\\w+)\\s.*");
+                Matcher matcher = pattern.matcher(da);
+                if (!matcher.find() || matcher.groupCount() < 2)
+                {
+                    // oh-oh
+                }
+                String oldFontName = matcher.group(1);
+                da = da.replaceFirst(oldFontName, fontName.getName());
+
+                textField.setDefaultAppearance(da);
+            }
+
+            switch (field.getFullyQualifiedName()){
+
+                case("Ancestry_Heritage"):{
+                    field.setValue( translateAncestry( field.getValueAsString() ) );
+
+                    break;
+                }
+
+            }
+
+        }
+
+        doc.save(newCharacterPath);
+
+    }
+
     public static void main(String[] args) throws Exception {
+
+        Reader reader = Files.newBufferedReader(Paths.get( Main.class.getResource("Ancestry.json").getPath().substring(1)));
+
+        ancestryDic = new Gson().fromJson(reader,new TypeToken<HashMap<String,String>>(){}.getType());
+
+        String newCharacterPath = Main.class.getResource("/").getPath() + "NewCharacter.pdf";
+
 
         //Файл, созданный PathBuilder
         File file = new File(Main.class.getResource("document.pdf").getPath());
@@ -46,10 +130,17 @@ public class Main {
         PDDocument doc2 = PDDocument.load(file2);
 
         // 3-ий параметр - файл, в который будет сохранён итог или который будет создан
-        listFields(doc, doc2,Main.class.getResource("/").getPath() + "NewCharacter.pdf");
+        listFields(doc, doc2,newCharacterPath);
+
+        File file3 = new File(newCharacterPath);
+        PDDocument doc3 = PDDocument.load(file3);
+
+        translate(doc3, newCharacterPath);
 
 
         System.out.println("Save to " +Main.class.getResource("/").getPath() + "NewCharacter.pdf");
+
+
 
     }
 
